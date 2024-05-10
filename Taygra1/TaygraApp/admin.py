336a9ee1d86e6888e61f12bb89cb896345e7b519
Matheus Beckerath <matheus.beckerath@hotmail.com>
2024-05-10 14:19:14 -0300

    feat: up models

--- Taygra1/TaygraApp/admin.py
+++ Taygra1/TaygraApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from TaygraApp.models import Cliente, Endereco, Carrinho, ItemCarrinho, Produto, SubCategoria,Categoria, ListaDesejos, ItemListaDesejos, Pagamento
+
+admin.site.register(Cliente)
+admin.site.register(Endereco)
+admin.site.register(Carrinho)
+admin.site.register(ItemCarrinho)
+admin.site.register(Produto)
+admin.site.register(SubCategoria)
+admin.site.register(Categoria)
+admin.site.register(ListaDesejos)
+admin.site.register(ItemListaDesejos)
+admin.site.register(Pagamento)
 
 # Register your models here.
